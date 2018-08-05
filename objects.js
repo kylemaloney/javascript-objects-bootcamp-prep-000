ca51d4bc38b1a77bb2919fd9ad3ff8cab08318c6
kylemaloney17@gmail.com <kylemaloney17@gmail.com>
2018-08-05 02:29:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object({artist: 'name'});
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist
 }
 function removeFromPlaylist(playlist, artistName, songTitle){
